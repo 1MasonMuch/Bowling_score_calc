4387f982e41aa5811f9db3700c1ec0b3089001b4
1MasonMuch <masonm126@nycstudents.net>
2026-09-25 14:02:10 -0400

    2

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const frames = [[]];
+const frames = [
+  { label: frame1, items: [1, 0] },
+  { label: frame2, items: [2, 3] },
+  { label: frame3, items: [2, 3] },
+  { label: frame4, items: [2, 3] },
+  { label: frame5, items: [2, 3] },
+  { label: frame6, items: [2, 3] },
+  { label: frame7, items: [2, 3] },
+  { label: frame8, items: [2, 3] },
+  { label: frame9, items: [2, 3] },
+  { label: frame10, items: [2, 3] },
+];
